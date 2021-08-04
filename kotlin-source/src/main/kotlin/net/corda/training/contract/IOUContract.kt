7d9ad262413b006a7e905408daf51b74f8228b87
Thiago Alvoravel <t.alvoravel@gmail.com>
2021-08-04 09:09:47 -0300

    Contract - Task 1

--- kotlin-source/src/main/kotlin/net/corda/training/contract/IOUContract.kt
+++ kotlin-source/src/main/kotlin/net/corda/training/contract/IOUContract.kt
@@ -2,6 +2,7 @@ package net.corda.training.contract
 
 import net.corda.core.contracts.CommandData
 import net.corda.core.contracts.Contract
+import net.corda.core.contracts.TypeOnlyCommandData
 import net.corda.core.contracts.requireSingleCommand
 import net.corda.core.transactions.LedgerTransaction
 import net.corda.training.state.IOUState
@@ -22,9 +23,7 @@ class IOUContract : Contract {
      * function to check for a number of commands which implement this interface.
      */
     interface Commands : CommandData {
-        // Add commands here.
-        // E.g
-        // class DoSomething : TypeOnlyCommandData(), Commands
+         class Issue : TypeOnlyCommandData(), Commands
     }
 
     /**
@@ -32,9 +31,6 @@ class IOUContract : Contract {
      * The constraints are self documenting so don't require any additional explanation.
      */
     override fun verify(tx: LedgerTransaction) {
-        // Add contract code here.
-        // requireThat {
-        //     ...
-        // }
+        tx.commands.requireSingleCommand<Commands>()
     }
 }
